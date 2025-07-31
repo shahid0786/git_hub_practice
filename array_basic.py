95ca8d04dba77558a9997cc2ebac927a03d66bc2
shahid0786 <47717138+shahid0786@users.noreply.github.com>
2025-07-31 10:34:39 +0530

    Update array_basic.py

--- array_basic.py
+++ array_basic.py
@@ -3,4 +3,5 @@ from array import *
 arr1 = array("i", [1,2,3,4,5,6])
 
 for i in arr1:
-    print("Printing array values one by one....",i)
+    print("Print the array values one by one....",i)
+    
