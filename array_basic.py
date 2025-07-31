bf6b2aa22fdd826be697fc2190689ffe07219bea
Shahid Shaikh <mohmshai@cisco.com>
2025-07-31 10:44:20 +0530

    Removed extra line

--- array_basic.py
+++ array_basic.py
@@ -3,9 +3,5 @@ from array import *
 arr1 = array("i", [1,2,3,4,5,6])
 
 for i in arr1:
-<<<<<<< HEAD
-    print("Printing array values here....",i)
-=======
     print("Print the array values one by one....",i)
     
->>>>>>> 95ca8d04dba77558a9997cc2ebac927a03d66bc2
